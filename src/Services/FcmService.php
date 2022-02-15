b750df2b68160eedf345f844b0985b80319bb4a9
Mohamed Eid <medoeid50@gmail.com>
2022-02-15 15:00:04 +0200

    wip

--- src/Services/FcmService.php
+++ src/Services/FcmService.php
@@ -4,13 +4,7 @@ namespace Bluex\Fcm\Serices;
 
 use Illuminate\Notifications\Notification;
 
-class FirebaseChannel
+class FcmService
 {
-    /**
-     * Send the given notification.
-     */
-    public function send($notifiable, Notification $notification)
-    {
-        $message = $notification->toFirebase($notifiable);
-    }
+
 }
